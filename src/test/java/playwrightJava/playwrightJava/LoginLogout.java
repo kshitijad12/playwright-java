be45c530839c40df10859931bce4607a8e0b89c1
kshitijad12 <kshitijadokarmare786@gmail.com>
2026-06-16 16:20:21 +0530

    add new files

---
+++ src/test/java/playwrightJava/playwrightJava/LoginLogout.java
@@ -0,0 +1,67 @@
+package playwrightJava.playwrightJava;
+
+import java.util.regex.Pattern;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
+public class LoginLogout {
+
+	public static void main(String[] args) {
+		
+		Browser br = null ;
+		Page page = null ;
+		
+		
+		try {
+		
+		br = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+				
+		page= br.newPage();
+		
+		page.navigate("https://freelance-learn-automation.vercel.app/login");
+		
+		PlaywrightAssertions.assertThat(page).hasTitle("Learn Automation Courses");
+		
+		//using id
+//		page.locator("#email1").fill("admin@email.com");
+		
+		//uing xpath 
+//		page.locator("xpath=//input[@name='email1']").fill("admin@email.com");
+		
+		//using css
+//		page.locator("css=input[name='email1']").fill("admin@email.com");
+		
+		
+		//using placeholder
+		page.getByPlaceholder("Enter Email").fill("admin@email.com");
+		
+		page.getByPlaceholder("Enter Password").fill("admin@123");
+		
+		//page.getByText("Sign in").nth(1).click();
+		
+		page.getByText("Sign in").last().click();
+		
+		PlaywrightAssertions.assertThat(page.locator(".welcomeMessage")).containsText("Welcome");
+		
+		page.getByAltText("menu").click();
+		
+		page.getByText("Sign out").click();
+		
+		PlaywrightAssertions.assertThat(page).hasURL(Pattern.compile("login"));
+		
+		}
+		finally {
+			
+			page.close();
+			
+			br.close();
+		}
+		
+
+	}
+
+}
---
+++ src/test/java/playwrightJava/playwrightJava/RegistrationForm.java
@@ -0,0 +1,56 @@
+package playwrightJava.playwrightJava;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
+public class RegistrationForm {
+
+	public static void main(String[] args) {
+		
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(800));
+		
+		Page page = browser.newPage();
+		
+		page.navigate("https://freelance-learn-automation.vercel.app/login");
+		
+		page.getByText("New user? Signup").click();
+		
+		PlaywrightAssertions.assertThat(page.locator(".submit-btn")).isDisabled();
+		
+		page.locator("#name").fill("kshitija dokarmare");
+		
+		page.locator("#email").fill("kshitijadokarmare786@gmail.com");
+		
+		page.getByPlaceholder("Password").fill("Kshitija@123");
+		
+		page.locator("xpath=//label[text()='PHP']//preceding::input[1]").click();
+		
+		PlaywrightAssertions.assertThat(page.locator("xpath=//label[text()='PHP']//preceding::input[1]")).isChecked();
+		
+		page.locator("xpath=//input[@value='Female']").click();
+		
+		PlaywrightAssertions.assertThat(page.locator("xpath=//input[@value='Female']")).isChecked();
+		
+        page.locator("#state").selectOption("Goa");
+        
+        String hobbies[] = {"Playing","Swimming"};
+        
+        page.locator("#hobbies").selectOption(hobbies);
+        
+		PlaywrightAssertions.assertThat(page.locator(".submit-btn")).isEnabled();
+		
+		page.locator(".submit-btn").click();
+		
+		page.waitForTimeout(5000);
+		
+		page.close();
+		
+		browser.close();
+        
+        
+	}
+
+}
