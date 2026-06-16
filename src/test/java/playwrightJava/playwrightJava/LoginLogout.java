10238af37aaaf65aad7fbb867e45c185eda8dab1
kshitijad12 <kshitijadokarmare786@gmail.com>
2026-06-16 16:32:45 +0530

    some changes initial setup

--- src/test/java/playwrightJava/playwrightJava/LoginLogout.java
+++ src/test/java/playwrightJava/playwrightJava/LoginLogout.java
@@ -14,8 +14,7 @@ public class LoginLogout {
 		
 		Browser br = null ;
 		Page page = null ;
-		
-		
+
 		try {
 		
 		br = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
